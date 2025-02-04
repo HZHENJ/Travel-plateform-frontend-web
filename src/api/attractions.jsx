c568372dcd9a23f1dd080d44a77baee7e6dc64d0
HUANG ZHENJIA <E1352450@u.nus.edu>
2025-02-04 18:39:25 +0800

    Completed attractions router

---
+++ src/api/attractions.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const CONTENT_LANGUAGE = import.meta.env.VITE_CONTENT_LANGUAGE;
+
+// fetch all hotels data from api
+export const fetchAttraction = async (offset = 0, limit = 6) => {
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/content/common/v2/search`, {
+        params: { 
+          offset, 
+          limit, 
+          dataset: "attractions" 
+        },
+        headers: {
+          "X-API-Key": API_KEY,
+          "X-Content-Language": CONTENT_LANGUAGE,
+        },
+      });
+      
+      return response;
+    } catch (error) {
+      console.error("fetchHotels", error);
+      throw error;
+    }
+  };
+
+// fetch hotel by uuid
+export const fetchAttractoionsByUUID = async(uuid) => {
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/content/attractions/v2/search`, {
+          params: {
+            searchType: "uuids",
+            searchValues: uuid
+          }, 
+          headers: {
+            "X-API-Key": API_KEY,
+            "X-Content-Language": CONTENT_LANGUAGE,
+          },
+        }
+      )
+      return response.data;
+    } catch (error) {
+      console.error("fetchHotelsByUUID", error);
+      throw error;
+    }
+  };
